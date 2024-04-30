fd0ba0945cafdc321aa32e598fa2f32589418f28
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-04-30 12:03:13 +0530

    Add layout files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import { Toaster } from "react-hot-toast";
+import Providers from "../components/Providers";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -20,9 +22,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <main className="mx-auto max-w-4xl p-4">
-          <Header />
-          {children}
-          <Footer />
+          <Providers>
+            <Toaster />
+            <Header />
+            {children}
+            <Footer />
+          </Providers>
         </main>
       </body>
     </html>
